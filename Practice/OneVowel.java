6f71816677143b785ba285f866b264b1f4f36887
Alexander Taylor <awtpi314@gmail.com>
2021-10-20 18:07:02 -0500

    first commit

---
+++ Practice/OneVowel.java
@@ -0,0 +1,33 @@
+package Practice;
+import java.util.Scanner;//scammer*
+
+public class OneVowel {
+
+    public static void main(String[] args) {
+        String vowels[] = { "a", "e", "i", "o", "u" };
+        int vowelOccurances[] = new int[5];
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Please input a word: ");
+        String input = sc.nextLine();
+        String[] split = input.split("");
+
+        for (String current : split) {
+            for (int i = 0; i < 5; i++) {
+                if (current.compareTo(vowels[i]) == 0 && vowelOccurances[i] == 0) {
+                    vowelOccurances[i]++;
+                }
+            }
+        }
+
+        if (vowelOccurances[0] + vowelOccurances[1] + vowelOccurances[2] + vowelOccurances[3]
+                + vowelOccurances[4] == 1) {
+            System.out.println("The word contains 1 unique vowel.");
+        } else {
+            System.out.println("The word does not contain 1 unique vowel.");
+        }
+
+        sc.close();
+    }
+}
---
+++ Practice/TipCalculator.java
@@ -0,0 +1,26 @@
+package Practice;
+import java.util.Scanner;
+
+public class TipCalculator {
+
+    public static double CalcTip(double s, double t, int p){
+        return Math.ceil((s*(1+t/100))/p * 100)/100;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the subtotal: $");
+        double subtotal = sc.nextDouble();
+        
+        System.out.print("Enter the tip: ");
+        double tip = sc.nextDouble();
+
+        System.out.print("Enter the party size: ");
+        int party_size = sc.nextInt();
+
+        System.out.println("The total for each person is $" + CalcTip(subtotal, tip, party_size));
+
+        sc.close();
+    }
+}
